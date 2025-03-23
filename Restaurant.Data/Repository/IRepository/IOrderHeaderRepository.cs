d657e2ad637108d8c769676a5f9ee3b4a70f4ef1
ndodley <ndodley@yahoo.com>
2025-03-23 14:02:48 -0400

    Set up order submissions. Redirect to cart after placing order.

---
+++ Restaurant.Data/Repository/IRepository/IOrderHeaderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Data.Repository.IRepository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+        void UpdateStatus(int id, string status);
+
+    }
+}
---
+++ Restaurant.Models/OrderHeader.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+
+
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Display(Name = "Order Total")]
+        public double OrderTotal { get; set; }
+
+
+        [Required]
+        [Display(Name = "Pick Up Time")]
+        public DateTime PickUpTime { get; set; }
+
+
+        [Required]
+        [NotMapped]
+        public DateTime PickUpDate { get; set; }
+
+
+        public string Status { get; set; }
+
+        public string? Comments { get; set; }
+
+        public string? TransactionId { get; set; }
+
+        // Stripe Properties
+        //public string? SessionId { get; set; }
+        //public string? PaymentIntentId { get; set; }
+
+
+        [Display(Name = "Pickup Name")]
+        [Required]
+        public string PickupName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
